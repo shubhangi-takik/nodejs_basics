2fe96d0c1cc7c09baafedef901b79a1791a55c64
shubhangi-takik <shubhangitakik@gmail.com>
2025-12-30 16:22:22 +0530

    Updated project files

--- htt_ser.js
+++ htt_ser.js
@@ -1,15 +1,16 @@
+/// make server using nodejs
+
 const http = require("http");
 //HTTP (HyperText Transfer Protocol) is a communication protocol that defines how data is sent and received between a client (browser/app) and a server over the internet.
 
 const server = http.createServer(function(req, res){
     res.end("hello from other side");
-})
+});
 
-let port = 3000;
+server.listen(3000);
 
-let ser = server.listen(port,function(){
-    console.log(`server is running on port ${port}`);
-})
 
-console.log(ser);
+
+
+
 
